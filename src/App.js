c8e559e0dfa3f4e3791c0151f1d6dedacff10bbd
1711shashank <1711shashank@gmail.com>
2023-03-20 10:58:31 +0530

    custom hooks for fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import './App.css';
-import { useState } from 'react';
-import PageContext from './Context/PageContext';
-import ExpenseDetails from './Components/ExpenseDetails';
-import Home  from './Components/Home';
+// import { useState } from 'react';
+// import PageContext from './Context/PageContext';
+// import ExpenseDetails from './Components/ExpenseDetails';
+// import Home from './Components/Home';
+import useFetch from './useFetch';
+
 
 function App() {
 
-  const [showExpenseDetails, setShowExpenseDetails] = useState(false);
+  // const [showExpenseDetails, setShowExpenseDetails] = useState(false);
+
+  const BASE_URL = 'https://reqres.in/api/users'
+  const { data :news, loading, error } = useFetch(BASE_URL);
+
 
   return (
     <div className="App">
-      <PageContext.Provider value={{ setShowExpenseDetails }}>
+
+      <h1> Custom Hooks </h1>
+      {loading && <h3>Loading</h3>}
+      {error && <h3> Error </h3>}
+      <div>
+        <pre>{JSON.stringify(news, undefined, 2)}</pre>
+      </div>
+
+
+
+      {/* <PageContext.Provider value={{ setShowExpenseDetails }}>
 
         {
           !showExpenseDetails
             ? <Home/>
             : <ExpenseDetails />
         }
-      </PageContext.Provider>
+      </PageContext.Provider> */}
     </div>
   );
 }
